a2e7377d94c76872bb0cca13ab266aac38c558fe
Jayendra Lal Joshi <Jayendralal.joshi@nielseniq.com>
2025-09-02 14:53:54 +0200

    add aside, add basic styles to it, adjust pageTitle and add contentDiv add contentDiv in template.html and change page title to 'To-Do' assign sizes for all major elements in styles.css and add content to aside add basic styles to aside and it's children

--- src/loadDOM.js
+++ src/loadDOM.js
@@ -16,4 +16,46 @@ export function loadDom(){
         header.appendChild(showLinksButton);
         header.appendChild(title);
     })();
+    (function loadAside(){
+        const aside = document.querySelector("aside");
+        const filterContainer = document.createElement("div");
+        filterContainer.classList.add("filterContainer");
+        const filterContainerTitle = document.createElement("h2");
+
+        filterContainerTitle.textContent="Filter";
+
+        const todayTasksLink  = document.createElement("a");
+        const importantTasksLink = document.createElement("a");
+        const nextSevenDaysTasksLink = document.createElement("a");
+        const unplannedTasksLink = document.createElement("a");
+        const allTasksLink = document.createElement("a");
+
+        todayTasksLink.textContent="Today";
+        importantTasksLink.textContent="Important";
+        nextSevenDaysTasksLink.textContent="Next 7 Days";
+        unplannedTasksLink.textContent="Unplanned";
+        allTasksLink.textContent="All Tasks";
+
+        filterContainer.appendChild(filterContainerTitle);
+        filterContainer.appendChild(todayTasksLink);
+        filterContainer.appendChild(importantTasksLink);
+        filterContainer.appendChild(nextSevenDaysTasksLink);
+        filterContainer.appendChild(unplannedTasksLink);
+        filterContainer.appendChild(allTasksLink);
+
+        aside.appendChild(filterContainer);
+
+        const projectContainer = document.createElement("div");
+        projectContainer.classList.add("projectContainer");
+        const projectContainerTitle = document.createElement("h2");
+        const addProjectButton = document.createElement("button");
+
+        projectContainerTitle.textContent="Projects";
+        addProjectButton.textContent="+ Add Project";
+
+        projectContainer.appendChild(projectContainerTitle);
+        projectContainer.appendChild(addProjectButton);
+
+        aside.appendChild(projectContainer);
+    })();
 }
